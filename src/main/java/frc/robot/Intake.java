90b22659779d46b7ccd47ec9b6d4008d856ca691
jmtayamada <96408404+jmtayamada@users.noreply.github.com>
2024-02-23 08:47:22 -0800

    added constructor that alows for passing in own Can Ids

--- src/main/java/frc/robot/Intake.java
+++ src/main/java/frc/robot/Intake.java
@@ -21,5 +21,35 @@ public class Intake {
         m_pitchEncoder = new CANcoder(IntakeConstants.kPitchEncoderCanId);
     }
 
-    
+    /**
+     * Constructor that uses passed in Can Ids
+     * @param IntakeMotorCanId
+     * @param PitchMotorCanId
+     * @param PitchEncoderCanId
+     */
+    public Intake(int IntakeMotorCanId, int PitchMotorCanId, int PitchEncoderCanId) {
+        m_intakeMotor = new CANSparkMax(IntakeMotorCanId, MotorType.kBrushless);
+        m_pitchMotor = new CANSparkMax(PitchMotorCanId, MotorType.kBrushless);
+        m_pitchEncoder = new CANcoder(PitchEncoderCanId);
+    }
+
+    private void lowerIntake() {
+        if (m_pitchEncoder.getAbsolutePosition().getValueAsDouble() > IntakeConstants.kPitchLow) {
+            m_pitchMotor.set(-1);
+        }
+    }
+
+    private void raiseIntake() {
+        if (m_pitchEncoder.getAbsolutePosition().getValueAsDouble() < IntakeConstants.kPitchHigh) {
+            m_pitchMotor.set(1);
+        }
+    }
+
+    private void runIntake() {
+        m_intakeMotor.set(1);
+    }
+
+    private void stopIntake() {
+        m_intakeMotor.set(0);
+    }
 }
